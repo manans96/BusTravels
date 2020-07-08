fa679eef50f0d28ea29842c778205ea3fb858caa
manans96 <sanghvimanan96@gmail.com>
2020-07-08 17:23:27 +0530

    Init work

---
+++ src/main/java/com/manan/busservice/BusserviceApplication.java
@@ -0,0 +1,13 @@
+package com.manan.busservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manan/busservice/BusserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manan.busservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
