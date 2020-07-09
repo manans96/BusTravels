21b038f87262a2eef4e86d109289862d0e4c88ca
manans96 <sanghvimanan96@gmail.com>
2020-07-09 15:48:50 +0530

    Added base package scanning in main

--- src/main/java/com/manan/busservice/main/BusserviceApplication.java
+++ src/main/java/com/manan/busservice/main/BusserviceApplication.java
@@ -2,9 +2,11 @@ package com.manan.busservice.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EntityScan(basePackages = {"com.manan.busservice.model"})				//to scan the base package of the entities if main is not present in the parent folder
 public class BusserviceApplication {
 
 	public static void main(String[] args) {
