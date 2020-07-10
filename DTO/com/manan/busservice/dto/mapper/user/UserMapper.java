5154445f80681d1eedeb5bb6c01393a25c7d2e38
manans96 <sanghvimanan96@gmail.com>
2020-07-10 19:25:48 +0530

    Remodeling User DTO

--- DTO/com/manan/busservice/dto/mapper/user/UserMapper.java
+++ DTO/com/manan/busservice/dto/mapper/user/UserMapper.java
@@ -1,8 +1,5 @@
 package com.manan.busservice.dto.mapper.user;
 
-import com.manan.busservice.dto.mapper.operations.BookingMapper;
-import com.manan.busservice.dto.mapper.operations.TicketMapper;
-import com.manan.busservice.dto.mapper.operator.BusOperatorMapper;
 import com.manan.busservice.dto.model.user.User;
 import com.manan.busservice.model.user.UserEntity;
 
@@ -16,11 +13,7 @@ public class UserMapper {
 				.setLastName(user.getLastName())
 				.setEmail(user.getEmail())
 				.setPhoneNo(user.getPhoneNo())
-				.setRole(user.getRole())
-				.setUserAuth(UserAuthMapper.toUserAuth(user.getUserAuth()))
-				.setTicket(TicketMapper.toTicket(user.getTicket()))
-				.setBooking(BookingMapper.toBooking(user.getBooking()))
-				.setOperator(BusOperatorMapper.toBusOperator(user.getOperator()));
+				.setRole(user.getRole());
 		
 	}
 
--- DTO/com/manan/busservice/dto/model/user/User.java
+++ DTO/com/manan/busservice/dto/model/user/User.java
@@ -1,11 +1,6 @@
 package com.manan.busservice.dto.model.user;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.manan.busservice.dto.model.operations.Booking;
-import com.manan.busservice.dto.model.operations.Ticket;
-import com.manan.busservice.dto.model.operator.BusOperator;
 import com.sun.istack.NotNull;
 
 import lombok.Data;
@@ -24,13 +19,5 @@ public class User {
 	@NotNull private String email;
 	@NotNull private String phoneNo;
 	@NotNull private String role;
-	
-	@NotNull private UserAuth userAuth;
-	
-	private List<Ticket> ticket;
-	
-	private List<Booking> booking;
-	
-	private BusOperator operator;
 
 }
