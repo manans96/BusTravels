102c0da4aa6c7efd5112e02c2dfbc78d56c873e9
manans96 <sanghvimanan96@gmail.com>
2020-07-13 11:29:06 +0530

    Created DateUtils

---
+++ Utility/com/manan/busservice/utility/DateUtils.java
@@ -0,0 +1,22 @@
+package com.manan.busservice.utility;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtils {
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("yyy-MM-dd");
+	
+	public static Date today() {
+		return new Date();
+	}
+	
+	public static String sdfToday() {
+		return sdf.format(today());
+	}
+	
+	public static String formattedDate(Date date) {
+		return date != null ? sdf.format(date) : sdfToday();
+	}
+
+}
